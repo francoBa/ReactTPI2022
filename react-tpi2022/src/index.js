51bba1c45eb6362dbb45f770a52a1e95710a2283
Franco Barreto <franco.barreto@gmail.com>
2022-07-04 17:41:51 -0300

    add tailwindcss antdesign sweetalert2

--- react-tpi2022/src/index.js
+++ react-tpi2022/src/index.js
@@ -16,10 +16,8 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-{
-  /* <Layout>
+/* <Layout>
     <Header />
     <Main />
     <Footer />
   </Layout> */
-}
---
+++ react-tpi2022/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  // constructor() {
+  //   super();
+
+  // }
+
+  // state = {
+  //   valor: 0,
+  // };
+
+  render() {
+    return <></>;
+  }
+}
+
+export default Home;
